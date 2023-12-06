0a4608254161d811e91080e377388e34e72327b7
Farouk <faroukwebdesign@gmail.com>
2023-12-06 19:34:52 +0100

    feat(sidebar and navbar): add basic ui of sidebar, add basic ui of navbar on mobile view
    implemented the basic ui of the sidebar on large screens , implemented basic ui for navbar in mobile
    view
    
    fixes #3

---
+++ src/components/Navbar/MobileMenu.jsx
@@ -0,0 +1,85 @@
+import { Menu, Transition } from "@headlessui/react"
+import Link from "next/link"
+import React, { Fragment } from "react"
+import { CiLinkedin } from "react-icons/ci"
+import { FaGithub } from "react-icons/fa"
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi"
+
+function MobileMenu() {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ")
+  }
+  return (
+    <Menu as="div" className="text-left block lg:hidden">
+      <div>
+        <Menu.Button className="w-full justify-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 btn-circle btn-ghost">
+          {({ open }) =>
+            //I used the Built-In state from Headless UI to harmonize between the two icons down below
+            open ? (
+              <HiOutlineX className="w-6 h-6" />
+            ) : (
+              <HiOutlineMenuAlt3 className="w-6 h-6" />
+            )
+          }
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute px-8 right-0 z-10 lg:mr-4 w-screen md:w-screen divide-y divide-gray-400 origin-top-right rounded-md bg-white text-black focus:outline-none py-2">
+          <div className="py-6 mb-2 flex flex-col gap-6">
+            <Menu.Item>
+              <Link
+                href="/"
+                className="block px-4 py-2 mb-2 text-center text-lg"
+              >
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link
+                href="/about"
+                className="block px-4 py-2 mb-2 text-center text-lg"
+              >
+                About
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link
+                href="/about"
+                className="block px-4 py-2 mb-2 text-center text-lg"
+              >
+                Projects
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link
+                href="/contact"
+                className="block px-4 py-2 mb-2 text-center text-lg"
+              >
+                Contact
+              </Link>
+            </Menu.Item>
+          </div>
+          <div className="flex gap-6 items-center justify-center py-2 text-black">
+            <Link className="btn btn-circle btn-ghost" href="/">
+              <FaGithub className="text-xl" />
+            </Link>
+            <Link className="btn btn-circle btn-ghost" href="/">
+              <CiLinkedin className="text-2xl" />
+            </Link>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
+export default MobileMenu
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,28 +1,44 @@
 import Image from "next/image"
 import React from "react"
+import NavigationMenu from "./NavigationMenu"
+import { FaGithub } from "react-icons/fa"
+import { CiLinkedin } from "react-icons/ci"
+import Link from "next/link"
 
 function SideBar() {
   return (
-    <section className="h-screen w-full bg-white flex flex-col py-2 px-4">
-      {/* Avatar Section */}
-      <div className="border-b border-gray-300 py-8">
-        <div className="flex md:flex-row flex-col items-center gap-2">
-          <figure className="w-16 h-16">
-            <Image
-              src="/images/avatar.jpg"
-              alt="avatar"
-              height={600}
-              width={600}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </figure>
-          <div className="flex flex-col">
-            <p className="font-bold text-black">Farouk Zemmouri</p>
-            <p className="text-xs text-gray-400">Front-End Developer</p>
-          </div>
+    <div className="flex flex-col gap-2 h-screen bg-white sticky p-4 border-r border-gray-300 items-center">
+      {/* Info Section */}
+      <div className="flex flex-row gap-2 py-8 border-b border-gray-300">
+        <figure className="h-16 w-16">
+          <Image
+            src="/images/avatar.jpg"
+            alt=""
+            height={600}
+            width={600}
+            priority
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </figure>
+        <div className="flex flex-col text-start justify-center">
+          <p className="font-bold text-black">Farouk Zemmouri</p>
+          <p className="font-light text-gray-400 text-xs">
+            Front-End Developer
+          </p>
         </div>
       </div>
-    </section>
+      {/* Navigation Links Section */}
+      <NavigationMenu />
+      {/* Socials */}
+      <div className="flex justify-evenly text-black">
+        <Link className="btn btn-circle btn-ghost" href="/">
+          <FaGithub className="text-xl" />
+        </Link>
+        <Link className="btn btn-circle btn-ghost" href="/">
+          <CiLinkedin className="text-2xl" />
+        </Link>
+      </div>
+    </div>
   )
 }
 
